f99c89f3fc8192ccf69fe2b55d2bad2c00ca8e72
n-alekhin <n.alekhin@g.nsu.ru>
2022-11-15 19:33:12 +0300

    add scripts

---
+++ core/src/main/java/com/SpringProject/core/controllers/ErrorController.java
@@ -0,0 +1,14 @@
+package com.SpringProject.core.controllers;
+
+import java.util.logging.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+/*
+@ControllerAdvice
+public class ErrorController {
+
+
+  }
+}*/
