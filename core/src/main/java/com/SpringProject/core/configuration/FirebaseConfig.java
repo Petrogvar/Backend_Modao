83e97d31b2918d1cc8e045dfa8581e5547913ff3
n-alekhin <n.alekhin@g.nsu.ru>
2023-05-10 03:00:05 +0300

    update password verification

--- core/src/main/java/com/SpringProject/core/configuration/FirebaseConfig.java
+++ core/src/main/java/com/SpringProject/core/configuration/FirebaseConfig.java
@@ -8,23 +8,23 @@ import java.io.IOException;
 import java.io.InputStream;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class FirebaseConfig {
-
-  @Bean
-  public FirebaseApp firebaseApp() throws IOException {
-    InputStream serviceAccount = getClass().getResourceAsStream("/path/to/resources/serviceAccountKey.json");
-
-    FirebaseOptions options = new FirebaseOptions.Builder()
-        .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-        .build();
-
-    return FirebaseApp.initializeApp(options);
-  }
-
-  @Bean
-  public FirebaseMessaging firebaseMessaging() throws IOException {
-    return FirebaseMessaging.getInstance(firebaseApp());
-  }
-}
+//
+//@Configuration
+//public class FirebaseConfig {
+//
+//  @Bean
+//  public FirebaseApp firebaseApp() throws IOException {
+//    InputStream serviceAccount = getClass().getResourceAsStream("/path/to/resources/serviceAccountKey.json");
+//
+//    FirebaseOptions options = new FirebaseOptions.Builder()
+//        .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+//        .build();
+//
+//    return FirebaseApp.initializeApp(options);
+//  }
+//
+//  @Bean
+//  public FirebaseMessaging firebaseMessaging() throws IOException {
+//    return FirebaseMessaging.getInstance(firebaseApp());
+//  }
+//}
